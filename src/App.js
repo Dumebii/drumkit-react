e7fe5f92131382b8701537ec3b8cf9be23887b1a
Dumebi Okolo <okolodumebi@gmail.com>
2022-12-04 18:57:17 +0100

    try useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react'
 
 function App() {
+
+  const firstRef = useRef();
+
+  function handleChange() {
+      firstRef.current.innerText="Hello, Dumebi"
+      console.log("Bitch, work!")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 ref={firstRef}
+      onClick={handleChange}>
+        Hello world!
+      </h1>
     </div>
   );
 }
